25709259bdff215383569d25bbacfa7bd9f17946
akash@maticui <akash@maticui.com>
2024-07-25 13:03:49 +0530

    user auth complete

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,6 +15,7 @@ const jwt = require("jsonwebtoken")
 const { checkEmpty } = require("../utils/checkEmpty")
 const Admin = require("../models/Admin")
 const sendEmail = require("../utils/email")
+const User = require("../models/User")
 
 exports.registerAdmin = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
@@ -116,4 +117,55 @@ exports.verifyOTP = asyncHandler(async (req, res) => {
 exports.logoutAdmin = asyncHandler(async (req, res) => {
     res.clearCookie("admin")
     res.json({ message: "Admin Logout Success" })
+})
+
+
+
+exports.registerUser = asyncHandler(async (req, res) => {
+    const { name, mobile, email, password, cpassword } = req.body
+
+    const { error, isError } = checkEmpty({
+        name, mobile, email, password, cpassword
+    })
+    if (isError) { return res.status(400).json({ message: "All fields required", error }) }
+    if (!validator.isEmail(email)) { return res.status(400).json({ message: "Invalid email" }) }
+    if (!validator.isMobilePhone(mobile, "en-IN")) { return res.status(400).json({ message: "Invalid Mobile", }) }
+    if (!validator.isStrongPassword(password)) { return res.status(400).json({ message: "Provide Strong Password", }) }
+    if (!validator.isStrongPassword(cpassword)) { return res.status(400).json({ message: "Provide Strong Confirm Password", }) }
+    if (password !== cpassword) { return res.status(400).json({ message: "Password Do not match", }) }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    await User.create({ name, mobile, email, password: hash })
+
+    res.json({ message: "User Register Success" })
+
+})
+exports.loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const { error, isError } = checkEmpty({ email, password })
+    if (isError) { return res.status(400).json({ message: "All fields required", error }) }
+
+    const result = await User.findOne({ email })
+    if (!result) {
+        return res.status(401).json({ message: "Email Not Found" })
+    }
+
+    const verify = await bcrypt.compare(password, result.password)
+    if (!verify) {
+        return res.status(401).json({ message: "Password Do Not Match" })
+    }
+
+    const token = jwt.sign({ userId: result._id }, process.env.JWT_KEY, { expiresIn: "180d" })
+
+    res.cookie("user", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 180
+    })
+    res.json({ message: "User Register Success" })
+})
+exports.logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie("User")
+    res.json({ message: "User Logout Success" })
 })
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,5 +7,11 @@ router
     .post("/verify-admin-otp", authController.verifyOTP)
     .post("/logout-admin", authController.logoutAdmin)
 
+    .post("/login-mobile-user", authController.loginUser)
+    .post("/register-mobile-user", authController.registerUser)
+    .post("/logout-mobile-user", authController.logoutUser)
+
+
+
 
 module.exports = router
