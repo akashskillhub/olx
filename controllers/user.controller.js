34fc9d10e8499f270056a1d9dc8b6a9a9d9143c5
akash@maticui <akash@maticui.com>
2024-07-26 16:03:35 +0530

    middleware test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,7 @@ const sendEmail = require("../utils/email")
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     // console.log(req.loggedInUser)
     const result = await User.findById(req.loggedInUser)
+    console.log(result);
     const otp = Math.floor(10000 + Math.random() * 900000)
     await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })
     const isSend = await sendEmail({ to: result.email, subject: "Verify Email", message: otp })
