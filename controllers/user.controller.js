d6487ea9050cbf790ff479de129ad2a6a33ff309
akash@maticui <akash@maticui.com>
2024-07-30 14:43:40 +0530

    user post fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -74,14 +74,14 @@ exports.verifyMobileOTP = asyncHandler(async (req, res) => {
 
 })
 exports.addPost = asyncHandler(async (req, res) => {
-    const { title, desc, price, images, location } = req.body
-    const { error, isError } = checkEmpty({ title, desc, price, images, location })
+    const { title, desc, price, images, location, category } = req.body
+    const { error, isError } = checkEmpty({ title, desc, price, images, location, category })
     if (isError) {
         return res.status(400).json({ message: "All Fields Required", error })
     }
     // 👇 modify this code to support cloudnary
 
-    await Posts.create({ title, desc, price, images, location, user: req.loggedInUser })
+    await Posts.create({ title, desc, price, images, location, category, user: req.loggedInUser })
     res.json({ message: "post create success" })
 
 })
